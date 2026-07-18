ca4c309dc3d6b05e651380624a50515f1c1c3c68
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-07-18 16:57:51 +0700

    up

---
+++ SE181979PE/SE181979DepartmentService/src/main/java/com/minhtriet/se3979/se181979departmentservice/Se181979DepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.minhtriet.se3979.se181979departmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se181979DepartmentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Se181979DepartmentServiceApplication.class, args);
+    }
+
+}
---
+++ SE181979PE/SE181979DepartmentService/src/test/java/com/minhtriet/se3979/se181979departmentservice/Se181979DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhtriet.se3979.se181979departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se181979DepartmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
