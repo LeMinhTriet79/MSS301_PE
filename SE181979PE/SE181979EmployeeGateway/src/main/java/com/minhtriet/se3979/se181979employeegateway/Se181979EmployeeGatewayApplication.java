820ecb10bcf783c6fd0350cef156708616a3ac49
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-07-18 16:41:52 +0700

    up

---
+++ SE181979PE/SE181979EmployeeGateway/src/main/java/com/minhtriet/se3979/se181979employeegateway/Se181979EmployeeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.minhtriet.se3979.se181979employeegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se181979EmployeeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Se181979EmployeeGatewayApplication.class, args);
+    }
+
+}
---
+++ SE181979PE/SE181979EmployeeGateway/src/test/java/com/minhtriet/se3979/se181979employeegateway/Se181979EmployeeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhtriet.se3979.se181979employeegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se181979EmployeeGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
