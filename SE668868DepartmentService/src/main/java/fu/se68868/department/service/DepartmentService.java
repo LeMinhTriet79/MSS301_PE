55ca21d23f6bb534e7a4236b0b0433276dc1ae1d
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-07-22 16:44:20 +0700

    up

--- SE668868DepartmentService/src/main/java/fu/se68868/department/service/DepartmentService.java
+++ SE668868DepartmentService/src/main/java/fu/se68868/department/service/DepartmentService.java
@@ -1,4 +1,10 @@
 package fu.se68868.department.service;
 
+import fu.se68868.department.dto.DepartmentDTO;
+import fu.se68868.department.entity.Department;
+
 public interface DepartmentService {
+
+
+    public DepartmentDTO createDept(DepartmentDTO feDto);
 }
