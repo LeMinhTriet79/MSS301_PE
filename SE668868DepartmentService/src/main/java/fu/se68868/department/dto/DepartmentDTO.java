d1e9f9f48f6543fe5b5e48eb3b2f7e5d5296ff77
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-07-22 16:25:44 +0700

    up

--- SE668868DepartmentService/src/main/java/fu/se68868/department/dto/DepartmentDTO.java
+++ SE668868DepartmentService/src/main/java/fu/se68868/department/dto/DepartmentDTO.java
@@ -1,11 +1,17 @@
 package fu.se68868.department.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Nationalized;
 
 import java.time.LocalDate;
@@ -13,23 +19,38 @@ import java.time.LocalDate;
 // class này dùng trong api CREAT, tạo mới 1 phòng ban (id, code, name,...)
 // DTO này được gửi dưới dạng JSON từ React, Postman, SwaggerUI lên Controller
 // DTO này xuất hiện trong HTTP REQUEST MESSAGE, được gửi từ Client lên Server
+//==============================================================================
+//class này dùng trong api CREATE, tạo mới 1 phòng ban (id, code, name,...)
+//DTO này đc gửi dưới dạng JSON từ React, Postman, SwaggerUI lên Controller
+//DTO này xuất hiện trong HTTP REQUEST MESSAGE, GỬI TỪ CLIENT LÊN SERVER
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class DepartmentDTO {
 
-    private Integer id; //nếu tạo mới thì không gửi ID từ client lên, nếu edit thì gửi từ service về client
+    private Integer id;   //nếu tạo mới thì ko gửi id từ client lên server
+    //nếu edit thì gửi từ server về client
+    @NotNull(message = "Code is required")
+    @Size(max = 10, message = "Code must not exceed 10 characters")
+    @Pattern(regexp = "^[A-Za-z0-9]+$", message = "Code only contains letters, digits")
+    private String code;  //10, required, có format chỉ chữ, số -> Regular Expression (regex)
 
-    private String name; // 50, required
+    @NotNull(message = "Name is required")
+    @Size(max = 50, message = "Name must not exceed 50 characters")
+    private String name;  //50, required
 
-    private String code; // 10, required
+    @Size(max = 100, message = "Location must not exceed 100 characters")
+    private String location; //100
 
-    private LocalDate effectiveDate;
+    @Pattern(regexp = "ACTIVE|INACTIVE|CLOSED", message = "Invalid status. It must be: ACTIVE or INACTIVE or CLOSED")
+    private String status;  //10 ACTIVE, INACTIVE, CLOSED
 
-
-    private String status;
-
-
-    private String location;
-
+    @JsonFormat(pattern = "dd/MM/yyyy")  //dd-MM-yyyy hh:mm:ss
+    private LocalDate effectiveDate;    //@DateTimeFormat dành cho <form> gửi lên server
 
     private Integer parentId;
+
+    //constructor, getter, setter, toString() => Lombok
 
 }
