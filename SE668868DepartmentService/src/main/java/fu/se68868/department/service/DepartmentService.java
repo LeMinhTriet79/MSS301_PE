8ee57c078c1fa6dc2f77fd39516003c55b5fc79e
LeMinhTriet79 <trietleminh7979@gmail.com>
2026-07-22 16:44:35 +0700

    up

--- SE668868DepartmentService/src/main/java/fu/se68868/department/service/DepartmentService.java
+++ SE668868DepartmentService/src/main/java/fu/se68868/department/service/DepartmentService.java
@@ -5,6 +5,18 @@ import fu.se68868.department.entity.Department;
 
 public interface DepartmentService {
 
+    //GỌI REPO ĐỂ CRUD TABLE Department
+    //NHƯNG TRONG TAY CỦA NÓ CHỈ CÓ DTO NHẬN TỪ CONTROLLER
+    //VÀ CONTROLLER NHẬN VỀ CŨNG CHỈ NHẬN DTO
+
+    //REPO THÌ LẠI CHỈ CHƠI VỚI ENTITY -> MAPPER XUẤT HIỆN: CONVERT DTO <-> ENTITY
+
+    //CÁC HÀM CRUD TABLE Department HERE; CHỈ CÓ TÊM HÀM HOY, IMPLMENTs BÊN CLASS Impl
+
+    //1. làm trước hàm createDept(nhận vào deptDto từ FE)
+    //                        -> return deptDto converted từ DB (có thêm key tự tăng)
 
     public DepartmentDTO createDept(DepartmentDTO feDto);
+
+    //TODO: còn hàm R.U.D
 }
